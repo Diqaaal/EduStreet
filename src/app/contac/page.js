7e36a8d30ad9a8078b04f0022653f3af78bae91b
Diqaaal <diqadran@gmail.com>
2024-03-07 00:10:57 +0700

    huh

--- src/app/contac/page.js
+++ src/app/contac/page.js
@@ -6,7 +6,7 @@ export default function contac() {
   return (
     <div>
       <div className=" flex justify-center items-center h-[700px] gap-9 bg-gradient-to-b from-[#D2E4FF] from-5% via-[#448EFE] via-60% to-[#FAFBFC]">
-        <div className=" w-[450px] p-4 bg-gray-400 rounded-xl bottom-2 border-[#F0E8E8] shadow-xl">
+        <div className=" w-[450px] p-4 bg-gray-200 rounded-xl bottom-2 border-[#F0E8E8] shadow-xl">
           <h1 className=" pb-2 font-bold text-xl">Butuh Bantuan dan Dukungan</h1>
           <p className=" text-xs">Jangan ragu untuk menghubungi kami kapan saja. kami akan membalas Anda sesegera mungkin!</p>
           <div className="flex p-8 cursor-pointer">
@@ -20,8 +20,8 @@ export default function contac() {
         </div>
         <div className=" flex flex-col justify-center items-center w-[500px]">
           <h1 className=" pb-4 font-extrabold text-4xl">Ada Pertanyaan?</h1>
-          <input type="email" className=" p-2 m-2 w-80 rounded-lg border-none "></input>
-          <input type="text" className=" p-2 m-2 w-80 rounded-lg border-none "></input>
+          <input type="email" placeholder="email" className=" p-2 m-2 w-80 rounded-lg border-none "></input>
+          <input type="text" placeholder="masukan saran atau bantuan" className=" p-2 m-2 w-80 rounded-lg border-none "></input>
           <button className=" w-80 p-2 font-bold text-xl bg-white rounded-xl border-2 hover:bg-orangeEdu hover:text-white">Kirim</button>
         </div>
       </div>
--- src/app/courses/page.js
+++ src/app/courses/page.js
@@ -1,5 +1,7 @@
 import Image from 'next/image';
 import courImg1 from '@/../../public/image/homeimg1.png';
+import bookImg from '@/../../public/image/book.jpg';
+import data from '@/components/data/courses.json';
 
 export default function Courses() {
   return (
@@ -8,6 +10,29 @@ export default function Courses() {
         <div className="">
           <Image src={courImg1} height={400} />
         </div>
+        <div className=" w-[50rem] m-5">
+          <h1 className=" py-4 text-4xl font-bold">Pelajari kursus kami yang membri hasil. Fokus Prakatik dan Portfolio Kamu.</h1>
+          <h3 className=" py-4 text-xl font-medium">Full Online dan Dipandu oleh Praktisi Senior. Praktikal, lebih dari sekadar Webinar. Fokus Bantu Kembangkan Skill dan Portfolio Ribuan Alumni.</h3>
+          <button className=" p-4 text-white text-lg font-bold bg-orangeEdu rounded-xl">Explore Courses</button>
+        </div>
+      </div>
+      <div className="courContainer flex justify-center items-center py-4  ">
+        {data.map((course) => {
+          return (
+            <div className="courCard w-64 h-96 p-4 m-5 bg-white rounded-lg border-2 border-[#F0E8E8] shadow-xl overflow-hidden" key={course.id}>
+              <div className=" flex justify-center">
+                <Image src={bookImg} width="150" alt="cover" />
+              </div>
+              <div className=" text-center relative">
+                <h1 className=" text-xl font-bold text-dopeEdu">{data.name}</h1>
+                <div className=" text-sm text-dopeEdu2/70 absolute right-0 left-0 top-20">
+                  <h3>{data.modul} Modul</h3>
+                  <h3>{data.siswa} Siswa</h3>
+                </div>
+              </div>
+            </div>
+          );
+        })}
       </div>
     </div>
   );

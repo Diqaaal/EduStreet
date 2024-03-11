c66d80578570d0a21c27ba1676fd97e1cb6143a8
Diqaaal <diqadran@gmail.com>
2024-03-11 08:29:05 +0700

    a

--- src/app/detail/page.js
+++ src/app/detail/page.js
@@ -1,20 +1,20 @@
-'use client';
-import { useRouter } from 'next/navigation';
-import data from '@/components/data/courses.json'; // Ubah dengan jalur file JSON yang sesuai
-import Image from 'next/image'; // Import Image jika Anda menggunakan Next.js Image component
+// 'use client';
+// import { useRouter } from 'next/navigation';
+// import data from '@/components/data/courses.json'; // Ubah dengan jalur file JSON yang sesuai
+// import Image from 'next/image'; // Import Image jika Anda menggunakan Next.js Image component
 
-export default function CardDetail() {
-  const router = useRouter();
-  const { id } = router.query;
-  const course = data.find((course) => course.id === parseInt(id));
+// export default function CardDetail() {
+//   const router = useRouter();
+//   const { id } = router.query;
+//   const course = data.find((course) => course.id === parseInt(id));
 
-  if (!course) {
-    return <div>Course not found</div>;
-  }
+//   if (!course) {
+//     return <div>Course not found</div>;
+//   }
 
-  return (
-    <div className=" bg-slate-500">
-      <h3>heloo{course.name}</h3>
-    </div>
-  );
-}
+//   return (
+//     <div className=" bg-slate-500">
+//       <h3>heloo{course.name}</h3>
+//     </div>
+//   );
+// }
